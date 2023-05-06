a225c302aac5af9261c49ce21b64337ee4fd9346
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2023-05-06 21:15:30 +0800

    上传

---
+++ VolcanoPrincess_StrangerJermaine/StrangerJermaine.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace xiaoye97
+{
+    [BepInPlugin(GUID, PluginName, VERSION)]
+    public class StrangerJermaine : BaseUnityPlugin
+    {
+        public const string GUID = "me.xiaoye97.plugin.VolcanoPrincess.StrangerJermaine";
+        public const string PluginName = "StrangerJermaine";
+        public const string VERSION = "1.0.0";
+
+        void Start()
+        {
+            Harmony.CreateAndPatchAll(typeof(StrangerJermaine));
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Person), "AddFavor")]
+        public static bool Person_AddFavor_Patch(Person __instance, ref int num)
+        {
+            if (__instance.enName == "42")
+            {
+                num = 0;
+            }
+            return true;
+        }
+    }
+}
